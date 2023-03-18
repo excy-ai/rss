1fae33cfaedf0d797aed9acc1f61dd7fbcf658ab
Danil Protsenko <dan.protsenko@gmail.com>
2023-03-18 19:06:58 +0300

    feat: setup testing env

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+let { compilerOptions } = require('./tsconfig.json');
+module.exports = {
+  moduleNameMapper: {
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  roots: ['./src'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.tsx',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  modulePaths: [compilerOptions.baseUrl],
+  verbose: true,
+};
