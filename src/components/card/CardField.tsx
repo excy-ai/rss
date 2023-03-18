0c9a66b55099e88b0d151f707c14d2fc9ed049b1
Danil Protsenko <dan.protsenko@gmail.com>
2023-03-18 14:31:02 +0300

    feat: add Card component

---
+++ src/components/card/CardField.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './CardField.css';
+
+type CardFieldState = Record<string, never>;
+
+interface CardFieldProps {
+  caption: string;
+  children: string | React.ReactNode;
+}
+
+class CardField extends React.Component<CardFieldProps, CardFieldState> {
+  render() {
+    return (
+      <p className="card__field">
+        <strong className="field__caption">{this.props.caption}: </strong>
+        <span className="field__text">{this.props.children}</span>
+      </p>
+    );
+  }
+}
+
+export default CardField;
