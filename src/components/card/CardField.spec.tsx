599c2b8558d48d5a458d40b38688c020e53d28e8
Danil Protsenko <dan.protsenko@gmail.com>
2023-03-19 14:05:59 +0300

    feat: add tests for components

---
+++ src/components/card/CardField.spec.tsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CardField from 'components/card/CardField';
+
+describe('CardField', () => {
+  const caption = 'testCaption';
+  const captionSearchText = /testCaption/i;
+  const children = 'testChildren';
+  it('should contain field with classes', async () => {
+    render(<CardField caption={caption}>{children}</CardField>);
+    const captionElement = screen.queryByText(captionSearchText);
+    expect(captionElement).toBeInTheDocument();
+    expect(captionElement).toHaveClass('field__caption');
+    const childrenElement = screen.queryByText(children);
+    expect(childrenElement).toBeInTheDocument();
+    expect(childrenElement).toHaveClass('field__text');
+  });
+});
