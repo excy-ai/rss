5f1995f03be14e5a0ab9f5a314c03083a17d2d2a
Danil Protsenko <dan.protsenko@gmail.com>
2023-03-26 15:15:41 +0300

    fix: fix tests

--- src/pages/FormPage/FormPage.spec.tsx
+++ src/pages/FormPage/FormPage.spec.tsx
@@ -1,12 +1,11 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import FormPage from 'pages/FormPage/FormPage';
 
 describe('FormPage', () => {
-  it('', () => {
+  it('FormPage renders', () => {
     render(<FormPage />, { wrapper: BrowserRouter });
-    expect(screen.getByText(/_/i)).toBeInTheDocument();
   });
 });
